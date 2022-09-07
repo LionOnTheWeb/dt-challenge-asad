7f6fad4d190e869b2a62b169ff38c57f9cf8beb1
Asad Richardson <asad@hasshdigital.com>
2022-09-07 02:57:56 -0400

    Added form component skeleton

--- src/lib/forms/signup.svelte
+++ src/lib/forms/signup.svelte
@@ -3,15 +3,44 @@
 </script>
 
 <style lang="postcss">
-	div {
-		background-color: red;
-		& h1 {
-			color: blue;
-		}
+	form {
+		text-align: left;
+		display: flex;
+		flex-flow: column wrap;
 	}
 
+	input {
+		/* display: block; */
+	}
+
+	span:first-child {
+		font-size: 1.2rem;
+	}
 </style>
 
-<div>
-<h1>Test</h1>
-</div>
+<span>Let's get started</span>
+<form action="">	
+	<label for="countryOfResidence">Country of residence</label>
+	<input name="countryOfResidence" type="location">
+
+	<label for="email">Email</label>
+	<input type="email" name="email" id="email">
+
+	<label for="password">Create your password</label>
+	<input type="password" name="password" id="password">
+
+	
+	<input 
+	type="password" 
+	name="passwordConfirmation" 
+	placeholder="Re-enter Password" 
+	aria-label="passwordConfirmation" 
+	id="passwordConfirmation">
+
+	<input type="checkbox" aria-labelledby="tac">
+	<span id="tac">I agree with the <a href="javascript:void(0);">Terms &amp; Conditions</a></span>
+
+	<input type="submit" value="Signup">
+</form>
+<a href="javascript:void(0);">Log In</a>
+<a href="javascript:void(0);">Customer Support</a>
