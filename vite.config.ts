d4b29b7a3e22bd26dc706847690ecbab17579298
Asad Richardson <asad@hasshdigital.com>
2022-09-15 14:18:10 -0400

    Final changes before sunsetting assignment, its been fun

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import * as path from 'path'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   	plugins: [svelte()],
-	css: { postcss: './postcss.config.js'}
+	css: { postcss: './postcss.config.js' },
+	resolve: {
+		alias: {
+			'$lib': path.resolve(__dirname, './src/lib'),
+		}
+	}
 })
